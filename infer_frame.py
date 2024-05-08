78c0789292efc7159b987c040fa5e096ef88040f
Panda-F <49586348+Panda-F@users.noreply.github.com>
2024-05-09 00:32:18 +0800

    迭代目标检测+融合算法(提速巨多)

--- infer_frame.py
+++ infer_frame.py
@@ -2,7 +2,7 @@ from mmdet.apis import DetInferencer
 import time
 
 # 初始化模型
-inferencer = DetInferencer(model='/home/fangzhou/Checkpoints/xiangsheng_0311_dino/dino-4scale_r50_8xb2-12e_coco.py', weights='/home/fangzhou/Checkpoints/xiangsheng_0311_dino/epoch_12.pth')
+inferencer = DetInferencer(model='/home/fangzhou/Checkpoints/xiangsheng_0311_dino/dino-4scale_r50_8xb2-12e_coco.py', weights='/home/fangzhou/Checkpoints/xiangsheng_0311_dino/epoch_12.pth', device="cuda:0")
 
 # 推理示例图片
 start_time = time.time()
